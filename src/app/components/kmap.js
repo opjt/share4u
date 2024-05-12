2f78fabb1c60c74c47ea986b06276d260671e722
opjt <jtpark1957@gmail.com>
2024-05-12 12:08:16 +0900

    커스텀 오버레이 적용

--- src/app/components/kmap.js
+++ src/app/components/kmap.js
@@ -1,132 +1,61 @@
 'use client'
 
 import { useEffect, useRef, useState } from "react";
-import Header from "./layout/header";
-import './kmap.css'
+import Header from "@/app/components/layout/header";
+import '@/app/components/kmap.css'
 import Axios from "@/util/axios";
+import useKakaoMap from "../hooks/useKakaoMap";
 import useCustomLogin from '@/app/hooks/useCustomLogin'
-let map;
 
-function placesSearchCB(data, status, pagination, setLocList) {
-    if (status === window.kakao.maps.services.Status.OK) {
-        console.log(data);
-
-
-        displayPlaces(data, setLocList);
-        console.log(pagination);
-    } else if (status === window.kakao.maps.services.Status.ZERO_RESULT) {
-        alert('검색 결과가 존재하지 않습니다.');
-        return;
-    } else if (status === window.kakao.maps.services.Status.ERROR) {
-        alert('검색 결과 중 오류가 발생했습니다.');
-        return;
-    }
-}
-
-
-
-function displayPlaces(places, setLocList) {
-    var bounds = new window.kakao.maps.LatLngBounds();
-    var placeList = [];
-    for (var i = 0; i < places.length; i++) {
-        var placePosition = new window.kakao.maps.LatLng(places[i].y, places[i].x);
-        bounds.extend(placePosition);
-
-        var marker = new window.kakao.maps.Marker({
-            map: map,
-            position: placePosition // 마커의 위치
-        });
-        //마커이름과 주소가 마커위에 생성되게 합니다.
-        // var iwContent = `<div style="padding:8px;text-overflow: ellipsis;overflow: hidden;white-space: nowrap;">` +
-        //     `<div style="font-weight: 600; margin-bottom: 3px;">${places[i].place_name}</div>` +
-        //     `<div>${places[i].address_name}</div>` +
-        //     `</div>`
-        const iwContent = `
-            <div class="wrap">
-                <div class="info">
-                    <div class="title">
-                        ${places[i].place_name}
-                        <div class="close" onclick="closeOverlay()" title="닫기"></div>
-                    </div>
-                    <div class="body">
-                
-                        <div class="desc">
-                            <div class="jibun ellipsis">${places[i].address_name}</div>
-                            <div class="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        `;
-            var overlay = new kakao.maps.CustomOverlay({
-                content: iwContent,
-                position: placePosition     
-            });
-        (function (marker, overlay) {
-            // 마커에 mouseover 이벤트를 등록하고 마우스 오버 시 인포윈도우를 표시합니다 
-            window.kakao.maps.event.addListener(marker, 'mouseover', function () {
-                overlay.setMap(map);
-            });
-
-            // 마커에 mouseout 이벤트를 등록하고 마우스 아웃 시 인포윈도우를 닫습니다
-            window.kakao.maps.event.addListener(marker, 'mouseout', function () {
-                overlay.setMap(null);
-            });
-        })(marker, overlay);
-
-
-        placeList.push({ place: places[i], marker: marker, infowindow:overlay })
-    }
-    console.log(placeList);
-    setLocList(placeList); // 업데이트된 마커 배열을 상태로 설정
-    map.setBounds(bounds);
-}
-function closeOverlay() {
-    overlay.setMap(null);     
-}
 
 export default function Kmap() {
-    const searchValue = useRef();
+  
+
+  const searchValue = useRef();
     const [locList, setLocList] = useState([]);
     const [userLocList, setUserLocList] = useState([])
     const [viewMode, setViewMode] = useState()
     const { isLogin,getUser } = useCustomLogin();
-    
 
-    useEffect(() => {
-        (async function () {
-            const res = await Axios.get(`/api/v1/loc`)
-            setUserLocList(res.data)
-        })();
-        const kakaoMapScript = document.createElement('script')
-        kakaoMapScript.async = false
-        kakaoMapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_JS_KEY}&autoload=false&libraries=services`
-        document.head.appendChild(kakaoMapScript)
+    const [container, setContainer ] = useState(null)
+    const {map, putMarker, kakao} = useKakaoMap(container)
 
-        const onLoadKakaoAPI = () => {
-            window.kakao.maps.load(() => {
-                var container = document.getElementById('map')
-                var options = {
-                    center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-                    level: 3,
-                }
-                map = new kakao.maps.Map(container, options); // 전역 map 변수에 할당
-            })
-        }
-
-        kakaoMapScript.addEventListener('load', onLoadKakaoAPI)
-    }, [])
-    useEffect(() => {
-  
-    }, [locList])
     const handleClickSearch = () => {
         console.log(searchValue.current.value);
-        //마커 초기화
+        // 마커 초기화
         for (var i = 0; i < locList.length; i++) {
             locList[i].marker.setMap(null);
         }
-        var ps = new window.kakao.maps.services.Places();
-        ps.keywordSearch(searchValue.current.value, (data, status, pagination) => placesSearchCB(data, status, pagination, setLocList));
+        setLocList([])
+        var ps = new kakao.maps.services.Places();
+        ps.keywordSearch(searchValue.current.value, (data, status, _pagination) => {
+          if (status === kakao.maps.services.Status.OK) {
+            // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+            // LatLngBounds 객체에 좌표를 추가합니다
+            const bounds = new kakao.maps.LatLngBounds()
+            let markers = []
+            console.log(data);
+            for (var i = 0; i < data.length; i++) {
+   
+              var position = new kakao.maps.LatLng(data[i].y, data[i].x)
+           
+              var {marker, overlay} = putMarker(position)
+              bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x))
+              markers.push({place: data[i], marker: marker, overlay:overlay})
+            }
+            
+            setLocList(markers)
+    
+            // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+            map.setBounds(bounds)
+          }  else if (status === kakao.maps.services.Status.ZERO_RESULT) {
+            alert('검색 결과가 존재하지 않습니다.');
+            return;
+        } else if (status === kakao.maps.services.Status.ERROR) {
+            alert('검색 결과 중 오류가 발생했습니다.');
+            return;
+        }
+        })
     }
     const handleSubmit = (event) => {
         event.preventDefault(); // 폼의 기본 동작인 전송을 막음
@@ -136,21 +65,18 @@ export default function Kmap() {
             console.log(res)
           })();
     }
-    const  closeOverlay = (placePosition) => {
-        var overlay = new kakao.maps.CustomOverlay({
-            position: placePosition     
-        });
-        overlay.setMap(null);     
-    }
+
     const handleClickPlace = (value) => {
-        console.log(value)
+        console.log(value.marker)
         for (var i = 0; i < locList.length; i++) {
-            locList[i].infowindow.setMap(null);
+            locList[i].overlay.setMap(null);
         }
+        
         map.panTo(value.marker.getPosition()); 
-        value.infowindow.setMap(map);
+        value.overlay.setMap(map);
 
     }
+  
     const handleClickHeart = async (place) => {
         // console.log(place)
         try {
@@ -165,71 +91,43 @@ export default function Kmap() {
 
     }
 
+  return (
 
-    return (
-        <>
-            <Header handleSubmit={handleSubmit} searchValue={searchValue} setViewMode={setViewMode}/>
-            <hr/>
-            <div className="flex ">
-                <div id="map" style={{ width: 'calc(100vw - 400px)', height: 'calc(100vh - 66px)' }}></div>
-                <div className="flex-1 overflow-auto" style={{ height: "calc(100vh - 66px)" }}>
-                    {/* 로그인되어있을 경우 유저 장소 표시 */}
-                    {isLogin && (
-                        <>
-                        {viewMode}
-                        </>
-                    )}
-                    <ul className="">
-                    {locList.map((value, index) => {
-                        return (
-                            <li key={index} className="p-2 flex gap-3 py-3 border-b-2" >
-                            <div className="skeleton w-24 h-24"></div>
-                            <div className="flex-1">
-                                <h2 className="font-bold text-lg cursor-pointer"  onClick={() => {handleClickPlace(value)}}>{value.place.place_name}</h2>
-                                <p className="text-sm text-gray-700">{value.place.address_name}</p>
-                                <p className="text-sm text-gray-700 overflow-hidden whitespace-nowrap text-ellipsis w-56">{value.place.category_name}</p>
-                                <div className="text-right pr-2"><div className={`badge badge-md ${userLocList.includes(value.place.id) ? 'bg-red-400 text-white' : 'border-red-400 text-red-400'}`} onClick={() => {handleClickHeart(value.place)}}>저장❤️</div></div>
-                            </div>
-                        </li>
-                        );   
-                    })}
-                    </ul>
+    <>
+      <Header handleSubmit={handleSubmit} searchValue={searchValue} setViewMode={setViewMode}/>
+      <hr/>
+      <div className="flex ">
+      <div style={{ width: 'calc(100vw - 400px)', height: 'calc(100vh - 66px)' }} ref={setContainer}></div>
 
-                </div>
-            </div>
+        <div className="flex-1 overflow-auto" style={{ height: "calc(100vh - 66px)" }}>
+          {/* 로그인되어있을 경우 유저 장소 표시 */}
+          {isLogin && (
+            <>
+              {viewMode}
+            </>
+          )}
+          <ul className="">
+            {locList.map((value, index) => {
+              return (
+                <li key={index} className="p-2 flex gap-3 py-3 border-b-2" >
+                  <div className="skeleton w-24 h-24"></div>
+                  <div className="flex-1">
+                    <h2 className="font-bold text-lg cursor-pointer" onClick={() => { handleClickPlace(value) }}>{value.place.place_name}</h2>
+                    <p className="text-sm text-gray-700">{value.place.address_name}</p>
+                    <p className="text-sm text-gray-700 overflow-hidden whitespace-nowrap text-ellipsis w-56">{value.place.category_name}</p>
+                    <div className="text-right pr-2"><div className={`badge badge-md ${userLocList.includes(value.place.id) ? 'bg-red-400 text-white' : 'border-red-400 text-red-400'}`} onClick={() => { handleClickHeart(value.place) }}>저장❤️</div></div>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
 
-            {/* <div className="map_wrap">
-                <div id="map" style={{ width: '100%', height: '100vh' }}></div>
+        </div>
+      </div>
 
-                <div id="menu_wrap" className="bg_white">
-                    <div className="option">
-                        <div>
-                            <form onSubmit={handleSubmit}>
-                                키워드 : <input type="text" ref={searchValue} id="keyword" size="15" />
-                                <button type="submit">검색하기</button>
-                            </form>
-                        </div>
-                    </div>
-                    <hr /> 
-				
-					<h1 className="text-3xl font-bold underline">
-						Hello world!
-						</h1>
-                    <ul id="placesList">
-						{locList.map((value, index) => {
-						return (
-							<li key={index} onClick={() => {handleClickPlace(value)}}>
-								<div>{value.place.place_name}</div>
-								<div>{value.place.address_name}</div>
-								<hr />
-							</li>
-							
-						);
-						})}
-					</ul>
-                    <div id="pagination"></div>
-                </div>
-            </div> */}
-        </>
-    );
+ 
+    </>
+  );
 }
+
+
--- src/app/hooks/useKakaoMap.js
+++ src/app/hooks/useKakaoMap.js
@@ -1,8 +1,46 @@
 
-import { useEffect,useState } from "react"
+import { useEffect, useState } from "react"
+function makeDiv(overlay) {
+    var content = document.createElement('div');
+    content.className = 'wrap';
+    var info = document.createElement('div');
+    info.className = 'info';
 
+    var title = document.createElement('div');
+    title.className = 'title';
+    title.textContent = 123123;
+
+    var closeBtn = document.createElement('div');
+    closeBtn.className = 'close';
+    closeBtn.title = '닫기';
+    closeBtn.onclick = function() {
+        overlay.setMap(null)
+    };
+
+    title.appendChild(closeBtn);
+    info.appendChild(title);
+
+    var desc = document.createElement('div');
+    desc.className = 'desc';
+
+    var jibun1 = document.createElement('div');
+    jibun1.className = 'jibun ellipsis';
+    jibun1.textContent = 123123;
+
+    var jibun2 = document.createElement('div');
+    jibun2.className = 'jibun ellipsis';
+    jibun2.textContent = '(우) 63309 (지번) 영평동 2181';
+
+    desc.appendChild(jibun1);
+    desc.appendChild(jibun2);
+    info.appendChild(desc);
+
+    content.appendChild(info);
+
+    return content;
+}
 const useKakaoMap = (mapContainer) => {
-    const [map,setMap] = useState(null)
+    const [map, setMap] = useState(null)
     const [kakao, setKakao] = useState(null)
     const [services, setServices] = useState(null)
 
@@ -11,9 +49,30 @@ const useKakaoMap = (mapContainer) => {
             map: map,
             position: position // 마커의 위치
         });
+
+        var overlay = new kakao.maps.CustomOverlay({
+            
+            position: position
+        });
+        var content = makeDiv(overlay)
+        
+        overlay.setContent(content)
+        // 마커에 mouseover 이벤트를 등록하고 마우스 오버 시 인포윈도우를 표시합니다 
+        // window.kakao.maps.event.addListener(marker, 'mouseover', function () {
+        //     overlay.setMap(map);
+        // });
+
+        // // 마커에 mouseout 이벤트를 등록하고 마우스 아웃 시 인포윈도우를 닫습니다
+        // window.kakao.maps.event.addListener(marker, 'mouseout', function () {
+        //     overlay.setMap(null);
+        // });
+        window.kakao.maps.event.addListener(marker, 'click', function () {
+            overlay.setMap(map);
+        });
+        return {marker:marker, overlay: overlay}
     }
     useEffect(() => {
-        if(!mapContainer) return
+        if (!mapContainer) return
 
         window.kakao?.maps.load(() => {
             console.log(mapContainer)
@@ -27,7 +86,7 @@ const useKakaoMap = (mapContainer) => {
         })
     }, [mapContainer])
     return {
-        map, putMarker
+        map, putMarker,services,kakao
     }
 }
 
